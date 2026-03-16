99a3a100f3b2cbc2369e7a6f1e53a297afd43cba
Shiny <cslinmiso@hotmail.com>
2026-03-16 22:57:45 +0800

    Update milwaukeetool-sign.py

--- milwaukeetool-sign.py
+++ milwaukeetool-sign.py
@@ -341,13 +341,8 @@ def process_account(account_info, index, total, failed_list):
             resp_json = response.json()
             signResult = format_sign_status(resp_json)
             print(f"{signResult}")
-            
-            SendKeyList = [key.strip() for key in SEND_KEY_LIST.split(',') if key.strip()]
-            print(f"🔧 SendKeyList共发现 {SendKeyList}")
-            # 确保长度一致
-            SendKeyList = SendKeyList[len(SendKeyList)]
-            
-            if signStatus == 200:
+        
+            if signStatus == 500:
                 print(f"📤 检测到有簽到，准备发送通知...{SEND_KEY_LIST}")
                 
                 response = send_msg_by_server(SEND_KEY_LIST, "milwaukeetool签到汇总", signResult)
